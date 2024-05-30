f14c5fba38b376c3536ee314ff6cace2567ae9bd
Idraak Mohd Khan <idraakmk@gmail.com>
2024-05-30 16:08:18 +0530

    Create digit_recognition.py

---
+++ digit_recognition.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# Loading dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Preprocess dataset
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32') / 255.0
+
+# Convert labels to one-hot encoding
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+# model building
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# model training
+model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
+
+# evaluating model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f'Test Accuracy: {accuracy}')
+
+# making predictions
+predictions = model.predict(X_test)
+
+# Visualization/Demonstration
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.imshow(X_test[i], cmap='gray')
+    plt.title(np.argmax(predictions[i]))
+    plt.axis('off')
+plt.show()
