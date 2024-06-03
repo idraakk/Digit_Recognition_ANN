2db4e152c6a957d97a4a912258360a1f89fe6cb5
Idraak Mohd Khan <idraakmk@gmail.com>
2024-06-03 19:01:46 +0530

    Update digit_recognition.py

--- digit_recognition.py
+++ digit_recognition.py
@@ -5,14 +5,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 
-# Loading dataset
+# loading dataset from mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Preprocess dataset
+# preprocess dataset
 X_train = X_train.astype('float32') / 255.0
 X_test = X_test.astype('float32') / 255.0
 
-# Convert labels to one-hot encoding
+# labels to one-hot encoding
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
 
@@ -28,14 +28,14 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 # model training
 model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
 
-# evaluating model
+# evaluating model for finding test accuracy
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Test Accuracy: {accuracy}')
 
 # making predictions
 predictions = model.predict(X_test)
 
-# Visualization/Demonstration
+# Visualization/Demonstration, some extra work :p ~_~ 
 plt.figure(figsize=(10, 10))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
